d8b02ebd142368df769e8770d9647b7ca302f73c
shimaa.albashir14@gmail.com <shimaa.albashir14@gmail.com>
2020-11-02 23:07:46 +0400

    built the react app and api

--- django-todo-react/backend/backend/urls.py
+++ django-todo-react/backend/backend/urls.py
@@ -24,5 +24,6 @@ router = routers.DefaultRouter()
 router.register(r'todos', views.TodoView, 'todo') 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),         path('api/', include(router.urls))            
+    path('admin/', admin.site.urls),         
+    path('api/', include(router.urls))            
 ]
--- django-todo-react/backend/todo/models.py
+++ django-todo-react/backend/todo/models.py
@@ -3,14 +3,24 @@
 from django.db import models
 # Create your models here.
 
-# add this
+class Categorize(models.Model):
+  name = models.CharField(max_length=50)
+
+  def __str__(self):
+    return "%s %s" % (self.name)
+
+
+
 class Todo(models.Model):
   title = models.CharField(max_length=120)
-  priority = models.IntegerField(choices=((1, ("week")),(2, ("Strong"))),
+  priority = models.IntegerField(choices=((1, ("low")),(2, ("high"))),
                                 default=1)
   description = models.TextField()
   completed = models.BooleanField(default=False)
   due_date = models.DateField()
+  categorize = models.ForeignKey(Categorize, on_delete=models.CASCADE)
+
 
   def _str_(self):
     return self.title
+
