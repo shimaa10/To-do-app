9790161ab883da4101ce4b5962cef28a4141b1aa
shimaa.albashir14@gmail.com <shimaa.albashir14@gmail.com>
2020-11-02 15:44:16 +0400

    create todo model and create migration file and apply change in database

--- django-todo-react/backend/todo/admin.py
+++ django-todo-react/backend/todo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Todo 
+
+class TodoAdmin(admin.ModelAdmin):  
+  list_display = ('title', 'description', 'completed','due_date','priority') 
+
+admin.site.register(Todo, TodoAdmin) 
--- django-todo-react/backend/todo/models.py
+++ django-todo-react/backend/todo/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+# todo/models.py
 
+from django.db import models
 # Create your models here.
+
+# add this
+class Todo(models.Model):
+  title = models.CharField(max_length=120)
+  priority = models.IntegerField(choices=((1, ("week")),(2, ("Strong"))),
+                                default=1)
+  description = models.TextField()
+  completed = models.BooleanField(default=False)
+  due_date = models.DateField()
+
+  def _str_(self):
+    return self.title
